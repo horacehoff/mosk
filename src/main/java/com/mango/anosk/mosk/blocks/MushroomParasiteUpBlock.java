a7069d8265036918a017ca2851543bfb8cc2d2cb
Anosk <78987310+Anousk@users.noreply.github.com>
2022-07-22 21:32:35 +0200

    Nearlly finished mushroom parasite imports.

--- src/main/java/com/mango/anosk/mosk/blocks/MushroomParasiteUpBlock.java
+++ src/main/java/com/mango/anosk/mosk/blocks/MushroomParasiteUpBlock.java
@@ -28,8 +28,35 @@ public class MushroomParasiteUpBlock extends HorizontalFacingBlock {
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext ctx) {
         Direction dir = state.get(FACING);
         switch(dir) {
+/*            case NORTH:
+                return VoxelShapes.union(
+                        createCuboidShape(-1, 12, 25, 1, 15, 26),//1
+                        createCuboidShape(-3, 12, 22, -2, 15, 24),
+                        createCuboidShape(-2, 12, 24, -1, 15, 25),
+                        createCuboidShape(10, 12, 22, 11, 15, 24),
+                        createCuboidShape(9, 12, 24, 10, 15, 25),
+                        createCuboidShape(7, 12, 25, 9, 15, 26),
+                        createCuboidShape(-1, 12, 12, 1, 15, 13),
+                        createCuboidShape(7, 12, 12, 9, 15, 13),
+                        createCuboidShape(10, 12, 14, 11, 15, 16),
+                        createCuboidShape(9, 12, 13, 10, 15, 14),//10
+                        createCuboidShape(-3, 12, 14, -2, 15, 16),
+                        createCuboidShape(-2, 12, 13, -1, 15, 14),
+                        createCuboidShape(1, 12, 11, 7, 15, 12),
+                        createCuboidShape(-4, 12, 16, -3, 15, 22),
+                        createCuboidShape(1, 12, 26, 7, 15, 27),
+                        createCuboidShape(-1, 12, 14, 9, 15, 24),
+                        createCuboidShape(-2, 12, 14, -1, 15, 24),
+                        createCuboidShape(-3, 12, 16, -2, 15, 22),
+                        createCuboidShape(1, 12, 25, 7, 15, 26),
+                        createCuboidShape(-1, 12, 24, 9, 15, 25),//20
+                        createCuboidShape(10, 12, 16, 11, 15, 22),
+                        createCuboidShape(9, 12, 14, 10, 15, 24),
+                        createCuboidShape(1, 12, 12, 7, 15, 13),
+                        createCuboidShape(-1, 12, 13, 9, 15, 14)//24
+                        );*/
             case NORTH:
-                return VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.5f);
+                return VoxelShapes.union(createCuboidShape(-4, 12, 5, 10, 15, 20));
             case SOUTH:
                 return VoxelShapes.cuboid(0.0f, 0.0f, 0.5f, 1.0f, 1.0f, 1.0f);
             case EAST:
