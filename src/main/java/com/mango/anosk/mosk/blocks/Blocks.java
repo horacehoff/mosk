2a7f182cfb441427a76e37f01bf3d58c9e5af3ed
Just_a_Mango <horace.hoff@gmail.com>
2022-07-22 17:15:06 +0200

    Added mushroom code

--- src/main/java/com/mango/anosk/mosk/blocks/Blocks.java
+++ src/main/java/com/mango/anosk/mosk/blocks/Blocks.java
@@ -9,9 +9,9 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Blocks {
-    public static final HupperBlock HUPPER_BLOCK = new HupperBlock(FabricBlockSettings.of(Material.METAL).hardness(3.0f));
+    public static final HupperBlock HUPPER_BLOCK = new HupperBlock(FabricBlockSettings.of(Material.METAL).hardness(3.0f).nonOpaque());
     public static final Block BIRCH_MUSHROOM = new Block(FabricBlockSettings.copyOf(net.minecraft.block.Blocks.MOSS_BLOCK));
-    public static final MushroomParasiteBlock MUSHROOM_PARASITE = new MushroomParasiteBlock(FabricBlockSettings.of(Material.SOLID_ORGANIC).hardness(0.2f));
+    public static final MushroomParasiteBlock MUSHROOM_PARASITE = new MushroomParasiteBlock(FabricBlockSettings.of(Material.SOLID_ORGANIC).hardness(0.2f).nonOpaque());
 
     public static void initBlocks() {
         Registry.register(Registry.BLOCK, new Identifier("mosk", "hupper_block"), HUPPER_BLOCK);
--- src/main/java/com/mango/anosk/mosk/items/Items.java
+++ src/main/java/com/mango/anosk/mosk/items/Items.java
@@ -2,6 +2,7 @@ package com.mango.anosk.mosk.items;
 
 import com.mango.anosk.mosk.blocks.Blocks;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -14,5 +15,6 @@ public class Items {
     public static void initItems() {
         Registry.register(Registry.ITEM, new Identifier("mosk", "rope_item"), ROPE_ITEM);
         Registry.register(Registry.ITEM, new Identifier("mosk", "hupper_item"), new BlockItem(Blocks.HUPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
+        Registry.register(Registry.ITEM, new Identifier("mosk", "mushroom_parasite_item"), new BlockItem(Blocks.MUSHROOM_PARASITE, new Item.Settings().group(ItemGroup.MISC)));
     }
 }
