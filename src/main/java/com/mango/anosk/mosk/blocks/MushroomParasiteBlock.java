534b45bbef3ade4e2c209d4d107678e1f2f6ea14
Anosk <78987310+Anousk@users.noreply.github.com>
2022-07-22 17:52:42 +0200

    Not rly useful its a test.

--- src/main/java/com/mango/anosk/mosk/blocks/MushroomParasiteBlock.java
+++ src/main/java/com/mango/anosk/mosk/blocks/MushroomParasiteBlock.java
@@ -12,17 +12,4 @@ public class MushroomParasiteBlock extends Block {
     public MushroomParasiteBlock(Settings settings) {
         super(settings);
     }
-
-    @Override
-    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context){
-        return VoxelShapes.union(
-                createCuboidShape(0.0f, 5.0f, 0.0f, 16.0, 6.0f, 16.0f),
-                createCuboidShape(14.0f, 0.0f, 0.0f, 16.0, 5.0f, 16.0f),
-                createCuboidShape(0.0f, 0.0f, 0.0f, 2.0, 5.0f, 16.0f),
-                createCuboidShape(2, 0, 0, 14, 5, 2),
-                createCuboidShape(2, 0, 14, 14, 5, 16),
-                createCuboidShape(4, 6, 4, 12, 12, 12),
-                createCuboidShape(6, 12, 6, 10, 16, 10)
-        );
-    }
 }
