e25f8dc135509f0edacb748663e43b2223a86332
Anosk <78987310+Anousk@users.noreply.github.com>
2022-07-22 23:12:33 +0200

    Flm.

--- src/main/java/com/mango/anosk/mosk/items/Items.java
+++ src/main/java/com/mango/anosk/mosk/items/Items.java
@@ -16,7 +16,5 @@ public class Items {
         Registry.register(Registry.ITEM, new Identifier("mosk", "rope_item"), ROPE_ITEM);
         Registry.register(Registry.ITEM, new Identifier("mosk", "hupper_item"), new BlockItem(Blocks.HUPPER_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE)));
         Registry.register(Registry.ITEM, new Identifier("mosk", "mushroom_parasite_item_up"), new BlockItem(Blocks.MUSHROOM_PARASITE_UP_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
-        Registry.register(Registry.ITEM, new Identifier("mosk", "mushroom_parasite_item_middle"), new BlockItem(Blocks.MUSHROOM_PARASITE_MIDDLE_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
-        Registry.register(Registry.ITEM, new Identifier("mosk", "mushroom_parasite_item_down"), new BlockItem(Blocks.MUSHROOM_PARASITE_DOWN_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
     }
 }
---
+++ src/main/java/com/mango/anosk/mosk/items/MushroomParasiteUpItem.java
@@ -0,0 +1,5 @@
+package com.mango.anosk.mosk.items;
+
+public class MushroomParasiteUpItem extends Items{
+
+}
